4bac0ea1b18505ab07ac0d421e6de0cad9995ba8
Yauhen <evgen.kuzmich@gmail.com>
2017-09-15 18:26:09 +0300

    Modified the Message class.

--- src/main/java/by/intexsoft/forum/entity/Message.java
+++ src/main/java/by/intexsoft/forum/entity/Message.java
@@ -1,18 +1,21 @@
 package by.intexsoft.forum.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import java.sql.Timestamp;
 
 import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Table(name = "messages")
 public class Message extends AbstractEntity {
+    @Column(nullable = false, length = 1000)
     public String text;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "created_by")
     public User createdBy;
+
+    @Column(name = "updated_at")
+    public Timestamp updatedAt;
 }
