e1724cb14fa707e5f67ca0de03c9eae43ae09f93
JenkaBY <evgen.kuzmich@gmail.com>
2017-09-17 15:41:52 +0300

    Deleted old classes.

--- src/main/java/by/intexsoft/forum/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package by.intexsoft.forum.service;
-
-import by.intexsoft.forum.entity.User;
-
-public interface UserService extends AbstractEntityService<User>{
-	User findByEmail(String email);
-}
