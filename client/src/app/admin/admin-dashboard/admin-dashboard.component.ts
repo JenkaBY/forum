4d7389719020c5563945a9267f3996ead8bf4269
JenkaBY <evgen.kuzmich@gmail.com>
2017-10-06 14:14:47 +0300

    Fixed error with navbar.

--- client/src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ client/src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'forum-admin-dashboard',
-  templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.css']
+    selector: 'forum-admin-dashboard',
+    templateUrl: './admin-dashboard.component.html',
+    styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
+    // sidenav: MatDrawer;
+    constructor() {
+    }
 
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+        // this.sidenav.open();
+    }
 
 }
