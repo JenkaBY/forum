4c8936a56294e8cf6462df28315daf23ae6aa3aa
Yauhen <evgen.kuzmich@gmail.com>
2017-10-18 12:30:48 +0300

    Created validation confirm password.

--- client/src/app/registration/registration-form/registration-form.component.ts
+++ client/src/app/registration/registration-form/registration-form.component.ts
@@ -6,6 +6,7 @@ import { Router } from "@angular/router";
 import IUserService from "../../service/interface/iuser.service";
 import { Account } from "../account";
 import { User } from "../../model/user";
+import { passwordsMatchValidator } from "../../shared/matched-passwords";
 
 @Component({
   selector: 'app-registration-form',
@@ -15,12 +16,14 @@ import { User } from "../../model/user";
 export class RegistrationFormComponent implements OnInit {
   accountForm: FormGroup;
   account: Account;
-  minNameLength = 4;
-  maxNameLength = 20;
-  minPasswordLength = 6;
-  maxPasswordLength = 15;
+  minNameLength = {value: 4};
+  maxNameLength = {value: 20};
+  minPasswordLength = {value: 6};
+  maxPasswordLength = {value: 16};
+  creating: boolean;
 
   ngOnInit() {
+    this.creating = false;
     this.account = new Account();
     this.initForm();
   }
@@ -31,31 +34,37 @@ export class RegistrationFormComponent implements OnInit {
   }
 
   onCreateAccount(): void {
+    this.creating = true;
     console.log(JSON.stringify(this.accountForm.value));
     this.convertFormToData();
     this.userService.create(this.convertAccountToUser())
       .then((user: User) => {
         console.log(JSON.stringify(user));
+        this.creating = false;
         this.redirectToInfoPage();
       })
-      .catch(error => console.log(error));
+      .catch(error => {
+        console.log(error);
+        this.creating = false;
+      });
   }
 
   private initForm(): void {
     this.accountForm = new FormGroup({
       'name': new FormControl('',
         [Validators.required,
-          Validators.minLength(this.minNameLength),
-          Validators.maxLength(this.maxNameLength)]),
+          Validators.minLength(this.minNameLength.value),
+          Validators.maxLength(this.maxNameLength.value)]),
       'email': new FormControl('',
         [Validators.required,
           Validators.email]),
       'password': new FormControl('',
         [Validators.required,
-          Validators.minLength(this.minPasswordLength),
-          Validators.maxLength(this.maxPasswordLength)]),
-      'passwordConfirm': new FormControl('', Validators.required)
-    })
+          Validators.minLength(this.minPasswordLength.value),
+          Validators.maxLength(this.maxPasswordLength.value)]),
+      'passwordConfirm': new FormControl('',
+        [Validators.required])
+    }, passwordsMatchValidator)
   }
 
   private convertFormToData(): void {
@@ -78,5 +87,26 @@ export class RegistrationFormComponent implements OnInit {
 
   onCancel(): void {
     this.location.back();
+  }
+
+  get name() {
+    return this.accountForm.get('name');
+  }
+
+  get email() {
+    return this.accountForm.get('email');
+  }
+
+  get password() {
+    return this.accountForm.get('password');
+  }
+
+  get passwordConfirm() {
+    return this.accountForm.get('passwordConfirm');
+  }
+
+  get passwordMismatch() {
+    return this.accountForm.errors && this.accountForm.errors['mismatch'] ?
+      this.accountForm.errors['mismatch'] : false;
   }
 }
